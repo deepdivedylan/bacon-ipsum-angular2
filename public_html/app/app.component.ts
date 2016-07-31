5802de5986cb996572350e99158596bb59d1f329
Dylan McDonald <dmcdonald21@cnm.edu>
2016-07-31 17:50:14 -0600

    checkpoint commit as new routing is written

--- public_html/app/app.component.ts
+++ public_html/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {Routes, Router, ROUTER_DIRECTIVES} from "@angular/router";
+import {HTTP_PROVIDERS} from "@angular/http";
+import {BaconComponent} from "./components/bacon-component";
 
 @Component({
 	selector: 'bacon-app',
-	templateUrl: './app/templates/splash.html'
+	templateUrl: './app/templates/splash.html',
+	directives: [ROUTER_DIRECTIVES],
+	providers: [HTTP_PROVIDERS]
 })
 
-export class AppComponent { }
+@Routes([
+	{path: "/bacon/", component: BaconComponent},
+	{path: "/bacon/:id", component: BaconComponent},
+	{path: "/bacon/:type", component: BaconComponent},
+	{path: "/bacon/:type/:id", component: BaconComponent},
+])
+
+export class AppComponent {
+	constructor(private router: Router) {}
+}
---
+++ public_html/app/components/bacon-component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Router} from '@angular/router';
+import {BaconService} from "../services/bacon-service";
+import {Bacon} from "../classes/bacon";
+
+@Component({
+	templateUrl: "app/templates/bacon.html",
+	providers: [BaconService]
+})
+
+export class BaconComponent implements OnInit {
+	constructor(private baconService: BaconService, private route: ActivatedRoute, private router: Router) {}
+
+	baconPlate: Bacon[];
+	errorMessage: string;
+	paragraphs: number;
+	type: string;
+
+	ngOnInit() {
+		this.route.params.subscribe(
+			params => {
+				this.paragraphs = +params["paragraphs"];
+				this.type = params["type"];
+				this.getBacon();
+			}
+		);
+	}
+
+	getBacon() {
+		if(this.paragraphs !== undefined && this.type !== undefined) {
+			this.baconService.getBaconByParagraphsAndType(this.paragraphs, this.type)
+				.subscribe(
+					Bacon => {
+						this.baconPlate = Bacon;
+					},
+					error => this.errorMessage = error
+				);
+		} else if(this.paragraphs === undefined && this.type !== undefined) {
+			this.baconService.getBaconByType(this.type)
+				.subscribe(
+					Bacon => {
+						this.baconPlate = Bacon;
+					},
+					error => this.errorMessage = error
+				);
+		} else if(this.paragraphs !== undefined && this.type === undefined) {
+			this.baconService.getBaconByParagraphs(this.paragraphs)
+				.subscribe(
+					Bacon => {
+						this.baconPlate = Bacon;
+					},
+					error => this.errorMessage = error
+				);
+		} else {
+			this.baconService.getBacon()
+				.subscribe(
+					Bacon => {
+						this.baconPlate = Bacon;
+					},
+					error => this.errorMessage = error
+				);
+		}
+	}
+}
